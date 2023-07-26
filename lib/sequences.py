2d89e575ec47681d9f465a91ffa15d0f4f8beb00
sharozecodes <126905674+sharozecodes@users.noreply.github.com>
2023-07-26 17:21:13 -0400

    Complete Lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
 
-def print_fibonacci(length):
-    pass
+def print_fibonacci(n):
+    if n == 0:
+        print([])
+    elif n == 1:
+        print([0])
+    else:
+        fib_seq = [0, 1]
+        while len(fib_seq) < n:
+            fib_seq.append(fib_seq[-1] + fib_seq[-2])
+        print(fib_seq)
